80ded466908f76ae8d52108d1b7e23211c0fd247
Frank Hossfeld <frank.hossfeld@googlemail.com>
2023-05-07 22:02:17 +0200

    fixed GitHub links

--- malio-processor/src/main/java/com/github/nalukit/malio/processor/ProcessorConstants.java
+++ malio-processor/src/main/java/com/github/nalukit/malio/processor/ProcessorConstants.java
@@ -22,20 +22,6 @@ import java.util.Date;
 
 public class ProcessorConstants {
 
-  //  public final static String META_DATA           = "MetaData";
-  //  public final static String META_INF            = "META-INF";
-  //  public final static String NALU_FOLDER_NAME    = "nalu-route";
-  //  public final static String PARAMETER_DELIMITER = "_pPp_";
-  //  @Deprecated // TODO remove next Iteration
-  //  public final static String TYPE_DELIMITER      = "_tTt_";
-  //  @Deprecated // TODO remove next Iteration
-  //  public final static String PROPERTIES_POSTFIX  = ".properties";
-  //  public final static String CREATOR_IMPL        = "CreatorImpl";
-  //  public final static String IMPL                = "Impl";
-  //  public final static String LOADER_IMPL         = "LoaderImpl";
-  //  public final static String MODULE_IMPL         = "ModuleImpl";
-  //  @Deprecated // TODO remove next Iteration
-  //  public final static String PLUGIN_IMPL         = "PluginImpl";
   public final static String PROCESSOR_VERSION = Malio.getVersion();
   public final static String BUILD_TIME        = new SimpleDateFormat("yyyy.MM.dd-HH:mm:ss").format(new Date(System.currentTimeMillis()));
 
